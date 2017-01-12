96e51fc3aa9085204d53f0899829200b0328e11f
David Griffiths <dgriff_67@yahoo.com>
2017-01-12 16:43:29 +0000

    installed django-allauth and then followed the set-up instructions. The set-up instructions included additions to settings.py and urls.py

--- growthstreet/settings.py
+++ growthstreet/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.linkedin',
 )
+
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,6 +76,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'growthstreet.wsgi.application'
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 
 # Database
