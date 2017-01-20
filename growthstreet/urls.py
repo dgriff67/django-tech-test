c2cbfdce4734103709e8f04cdc129451cc670ecb
David Griffiths <dgriff_67@yahoo.com>
2017-01-20 15:46:30 +0000

    /profile urls handled in the customer app

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from customer import urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^profile/', include('customer.urls')),
 ]
