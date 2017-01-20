4e1be599e0b4ac43005f877fb4e4ed554f5b8645
David Griffiths <dgriff_67@yahoo.com>
2017-01-20 15:48:31 +0000

    basic initial profile view to render customer profile

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Customer
 
 # Create your views here.
+def profile(request):
+    current_customer = request.user
+    data_dict = {'username':current_customer.username}
+    return render(request, 'customer/profile.html', data_dict)
