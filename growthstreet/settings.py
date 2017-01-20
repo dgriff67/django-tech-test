8c5817d5b9d36bd66a72676187e3376768d3b065
David Griffiths <dgriff_67@yahoo.com>
2017-01-20 12:02:21 +0000

    seetings.py and urls.py edited to enable authentication via social network linkedin using allauth

--- growthstreet/settings.py
+++ growthstreet/settings.py
@@ -31,12 +31,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # The Django sites framework is required
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # Social Proviers
+    'allauth.socialaccount.providers.linkedin',
+    # Regular Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
     'customer',
 )
 
@@ -68,6 +78,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+# For allauth
+SITE_ID = 1
 
 WSGI_APPLICATION = 'growthstreet.wsgi.application'
 
