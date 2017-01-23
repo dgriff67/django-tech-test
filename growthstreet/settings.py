768eb3c87b2817a50eb6a08fe44566cb555b8e27
David Griffiths <dgriff_67@yahoo.com>
2017-01-23 18:49:43 +0000

    ACCESS_TOKEN placed in settings.py

--- growthstreet/settings.py
+++ growthstreet/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
-
+ACCESS_TOKEN='D0G07oUEiFLOGRGpYwbJRdd59PCNmZM435c0zL0M'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
