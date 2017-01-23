11d9f1c4c27234353776cce4eeefbed3f63f30d3
David Griffiths <dgriff_67@yahoo.com>
2017-01-23 18:48:34 +0000

    now importing ACCESS_TOKEN from Settings. Also now check whether total_results is 0 or greater than zero and return responses appropriately

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.conf import settings
 from .models import Customer
+from django.http import HttpResponse
+import chwrapper
 
 # Create your views here.
 def profile(request):
     current_customer = request.user
-    return render(request, 'customer/profile.html', {'current_customer':current_customer})
+    if request.method == 'GET':
+        return render(request, 'customer/profile.html', {'current_customer':current_customer})
+    elif request.method == 'POST':
+        # search_term = request.POST.get("q", "")
+        search_client = chwrapper.Search(settings.ACCESS_TOKEN)
+        response = search_client.search_companies(request.POST.get("q", ""))
+        response_json = response.json()
+        if response_json['total_results'] == 0:
+            return HttpResponse('No results')
+        elif response_json['total_results'] > 0:
+            title = response_json['items'][0]['title']
+            return HttpResponse(title)
