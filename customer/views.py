185e4cb518daa392732829d531144da9532ca1ac
David Griffiths <dgriff_67@yahoo.com>
2017-01-23 14:30:20 +0000

    simplified implementation of profile action to return request .user

--- customer/views.py
+++ customer/views.py
@@ -3,6 +3,4 @@ from .models import Customer
 
 # Create your views here.
 def profile(request):
-    current_customer = request.user
-    data_dict = {'username':current_customer.username}
-    return render(request, 'customer/profile.html', data_dict)
+    return render(request, 'customer/profile.html', request.user)
