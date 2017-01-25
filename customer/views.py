1a8bd507cf284da964506a0d1bd50a1f11b1f0f0
David Griffiths <dgriff_67@yahoo.com>
2017-01-25 17:26:36 +0000

    profile view now handles both get and post requests. The post request is confirmation by the customer that the company is correct

--- customer/views.py
+++ customer/views.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
 from django.conf import settings
-from .models import Customer
+from .models import Customer, Company
 from django.http import HttpResponse
 import chwrapper
 
 # Create your views here.
 def profile(request):
     current_customer = request.user
-    query = request.GET.get("q")
-    if query:
-        # search_term = request.POST.get("q", "")
-        search_client = chwrapper.Search(settings.ACCESS_TOKEN)
+    search_client = chwrapper.Search(settings.ACCESS_TOKEN)
+    if request.method == 'GET':
+        query = request.GET.get("q")
+        if query:
+            # search_term = request.POST.get("q", "")
+            response = search_client.search_companies(query)
+            response_json = response.json()
+            if response_json['total_results'] == 0:
+                message = 'No results'
+                return render(request, 'customer/profile.html', {'current_customer':current_customer, 'message': message})
+            elif response_json['total_results'] > 0:
+                company_address = response_json['items'][0]['address']
+                title = response_json['items'][0]['title']
+                company_number = response_json['items'][0]['company_number']
+                return render(request, 'customer/profile.html', {'current_customer':current_customer, 'title': title, 'company_number': company_number, 'company_address': company_address})
+
+        else:
+            return render(request, 'customer/profile.html', {'current_customer':current_customer})
+    else: #request.method == 'POST'
+        query = request.POST.get('company_number')
         response = search_client.search_companies(query)
         response_json = response.json()
         if response_json['total_results'] == 0:
-            message = 'No results'
+            message = 'Something has gone wrong' + company_number
             return render(request, 'customer/profile.html', {'current_customer':current_customer, 'message': message})
-        elif response_json['total_results'] > 0:
-            company_number = response_json['items'][0]['company_number']
-            title = response_json['items'][0]['title']
-            address_dict = response_json['items'][0]['address']
-            return render(request, 'customer/profile.html', {'current_customer':current_customer, 'company_number': company_number, 'title': title, 'address_dict': address_dict})
-
-    else:
-        return render(request, 'customer/profile.html', {'current_customer':current_customer})
+        else:
+            company_dict = response_json['items'][0]['address']
+            company_dict['title'] = response_json['items'][0]['title']
+            company_dict['company_number'] = response_json['items'][0]['company_number']
+            c = Company(**company_dict)
+            c.save()
+            current_customer.company = c
+            current_customer.save()
+            message = 'Company confirmed'
+            return render(request, 'customer/profile.html', {'current_customer':current_customer, 'message': message, 'company_dict': company_dict})
