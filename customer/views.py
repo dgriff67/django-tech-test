1a3df42c4c41317b2b73b714478eaab2f4f64f18
David Griffiths <dgriff_67@yahoo.com>
2017-01-25 14:06:32 +0000

    profile view handles get requests either initial or after user clicking submit - now also a get request. If the user submits a search request the response is now sent to profile template with differentiation between 0 or greater than 0 results

--- customer/views.py
+++ customer/views.py
@@ -7,15 +7,20 @@ import chwrapper
 # Create your views here.
 def profile(request):
     current_customer = request.user
-    if request.method == 'GET':
-        return render(request, 'customer/profile.html', {'current_customer':current_customer})
-    elif request.method == 'POST':
+    query = request.GET.get("q")
+    if query:
         # search_term = request.POST.get("q", "")
         search_client = chwrapper.Search(settings.ACCESS_TOKEN)
-        response = search_client.search_companies(request.POST.get("q", ""))
+        response = search_client.search_companies(query)
         response_json = response.json()
         if response_json['total_results'] == 0:
-            return HttpResponse('No results')
+            message = 'No results'
+            return render(request, 'customer/profile.html', {'current_customer':current_customer, 'message': message})
         elif response_json['total_results'] > 0:
+            company_number = response_json['items'][0]['company_number']
             title = response_json['items'][0]['title']
-            return HttpResponse(title)
+            address_dict = response_json['items'][0]['address']
+            return render(request, 'customer/profile.html', {'current_customer':current_customer, 'company_number': company_number, 'title': title, 'address_dict': address_dict})
+
+    else:
+        return render(request, 'customer/profile.html', {'current_customer':current_customer})
