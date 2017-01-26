14603dd6e5aa66b6c9d5bbd8f39b04425e0a3e5d
David Griffiths <dgriff_67@yahoo.com>
2017-01-26 18:21:19 +0000

    added address model to admin section

--- customer/admin.py
+++ customer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Company
+from .models import Customer, Company, Address
 
 admin.site.register(Customer)
 admin.site.register(Company)
+admin.site.register(Address)
