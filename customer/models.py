30af47bfcbad411b2438532c0f0f7170610f3db5
David Griffiths <dgriff_67@yahoo.com>
2017-01-26 18:22:16 +0000

    added Address model

--- customer/models.py
+++ customer/models.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class Customer(AbstractUser):
     phone_number = models.CharField(max_length=100, null=True, blank=True)
     confirmed_phone_number = models.BooleanField(default=False)
-
     confirmed_name = models.BooleanField(default=False)
-
     company = models.ForeignKey(
         'Company', null=True
     )
@@ -31,8 +29,21 @@ class Company(models.Model):
     business_sector = models.CharField(
         max_length=100, choices=SECTORS, blank=True, null=True
     )
+    address = models.ForeignKey(
+        'Address', null=True
+    )
 
     title = models.CharField(max_length=200, null=True)
+    company_number = models.CharField(
+        max_length=8, null=True, blank=True,
+    )
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{0} - {1}, {2}".format(self.company_number, self.title, self.address)
+
+
+class Address(models.Model):
     care_of_name = models.CharField(max_length=50, null=True)
     premises = models.CharField(max_length=200, null=True)
     address_line_1 = models.CharField(max_length=200, null=True)
@@ -42,10 +53,6 @@ class Company(models.Model):
     locality = models.CharField(max_length=200, null=True)
     region = models.CharField(max_length=200, null=True)
     postal_code = models.CharField(max_length=100, null=True)
-    company_number = models.CharField(
-        max_length=8, null=True, blank=True,
-    )
-    verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return "{0} - {1}, {2}, {3}, {4}, {5}".format(self.company_number, self.title, self.premises, self.address_line_1, self.locality, self.postal_code)
+        return "{0}, {1}, {2}, {3}".format(self.premises, self.address_line_1, self.locality, self.postal_code)
