ffea1da602e048b9157f98d6ba0e7e795ea1ddb4
David Griffiths <dgriff_67@yahoo.com>
2017-01-27 12:23:30 +0000

    now redirect landing urls to profile view

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 from customer import urls
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='profile')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^profile/', include('customer.urls')),
