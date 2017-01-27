145586d17592216009f3f8d48af5e28a7202a686
David Griffiths <dgriff_67@yahoo.com>
2017-01-27 16:24:34 +0000

    added urls for loan app

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 from customer import urls
+from loan import urls
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='profile')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^profile/', include('customer.urls')),
+    url(r'^loan/', include('loan.urls')),
 ]
